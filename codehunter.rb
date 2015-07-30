6412dd7783a8f778de54b2b2925ae9bc7b24ed3f
rd135 <30534@naver.com>
2015-07-30 17:58:39 +0900

    Update codehunter.rb

--- codehunter.rb
+++ codehunter.rb
@@ -96,3 +96,17 @@ def SimpleSymbols(str)
          
 end
 
+#codebyte_8
+def CheckNums(num1,num2)
+
+  if(num1 == num2)
+    return '-1'
+  elsif(num1 < num2)
+    return 'true'
+  end 
+  
+  return 'false' 
+         
+end
+
+
